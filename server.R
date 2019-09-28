f32403780cde418e8a4a7ba1ea344f7e8b239c9b
iamnikhithabhat <47333294+iamnikhithabhat@users.noreply.github.com>
2019-09-28 13:47:16 +0530

    Update server.R

--- server.R
+++ server.R
@@ -1,48 +1,42 @@
-#
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
-library(ggplot2)
-library(dplyr)
-# select columns to be used in the analysis
-diam <- diamonds[,c(1:4,7)]
-# Define server logic required to draw a plot
+library(datasets)
+
+mpgData <- mtcars
+mpgData$am <- factor(mpgData$am, labels = c("Automatic", "Manual"))
+
 shinyServer(function(input, output) {
-    output$distPlot <- renderPlot({
-        # select diamonds depending of user input
-        diam <- filter(diamonds, grepl(input$cut, cut), grepl(input$col, color), grepl(input$clar, clarity))
-        # build linear regression model
-        fit <- lm( price~carat, diam)
-        # predicts the price 
-        pred <- predict(fit, newdata = data.frame(carat = input$car,
-                                                  cut = input$cut,
-                                                  color = input$col,
-                                                  clarity = input$clar))
-        # Drow the plot using ggplot2
-        x <- ggplot(data=diam, aes(x=carat, y = price))+
-            geom_point(aes(color = cut), alpha = 0.3)+
-            geom_smooth(method = "lm")+
-            geom_vline(xintercept = input$car, color = "red")+
-            geom_hline(yintercept = pred, color = "green")
-       x
+  
+  formulaText <- reactive({
+    paste("mpg ~", input$variable)
+  })
+  
+  formulaTextPoint <- reactive({
+    paste("mpg ~", "as.integer(", input$variable, ")")
+  })
+  
+  fit <- reactive({
+    lm(as.formula(formulaTextPoint()), data=mpgData)
+  })
+  
+  output$caption <- renderText({
+    formulaText()
+  })
+  
+  output$mpgBoxPlot <- renderPlot({
+    boxplot(as.formula(formulaText()), 
+            data = mpgData,
+            outline = input$outliers)
+  })
+  
+  output$fit <- renderPrint({
+    summary(fit())
+  })
+  
+  output$mpgPlot <- renderPlot({
+    with(mpgData, {
+      plot(as.formula(formulaTextPoint()))
+      abline(fit(), col=2)
     })
-    output$result <- renderText({
-        # renders the text for the prediction below the graph
-        diam <- filter(diamonds, grepl(input$cut, cut), grepl(input$col, color), grepl(input$clar, clarity))
-        fit <- lm( price~carat, diam)
-        pred <- predict(fit, newdata = data.frame(carat = input$car,
-                                                  cut = input$cut,
-                                                  color = input$col,
-                                                  clarity = input$clar))
-        res <- paste(round(pred, digits = 2), "$")
-        res
-    })
+  })
   
 })
-
