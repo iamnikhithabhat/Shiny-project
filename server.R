5ab69251eca4d704912817acce55627bd744de7d
iamnikhithabhat <47333294+iamnikhithabhat@users.noreply.github.com>
2019-10-11 11:44:32 +0530

    Update server.R

--- server.R
+++ server.R
@@ -1,42 +1,31 @@
-library(shiny)
-library(datasets)
+#
+# This is the server logic of a Shiny web application. You can run the 
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
 
-mpgData <- mtcars
-mpgData$am <- factor(mpgData$am, labels = c("Automatic", "Manual"))
+library(shiny)
+data(mtcars)
+mt <- as.data.frame(mtcars)
+mtFit <- lm(mpg ~ am + cyl + hp + wt, data = mt)
 
 shinyServer(function(input, output) {
-  
-  formulaText <- reactive({
-    paste("mpg ~", input$variable)
+  mtPred <- reactive({
+    amInput <- input$am
+    cylInput <- input$cyl
+    hpInput <- input$hp
+    wtInput <- input$wt
+    predict(mtFit, newdata = data.frame(
+      am = as.integer(amInput),
+      cyl = as.integer(cylInput),
+      hp = hpInput,
+      wt = wtInput
+    ))
   })
-  
-  formulaTextPoint <- reactive({
-    paste("mpg ~", "as.integer(", input$variable, ")")
+  output$mpgPred <- renderText({
+    mtPred()
   })
-  
-  fit <- reactive({
-    lm(as.formula(formulaTextPoint()), data=mpgData)
-  })
-  
-  output$caption <- renderText({
-    formulaText()
-  })
-  
-  output$mpgBoxPlot <- renderPlot({
-    boxplot(as.formula(formulaText()), 
-            data = mpgData,
-            outline = input$outliers)
-  })
-  
-  output$fit <- renderPrint({
-    summary(fit())
-  })
-  
-  output$mpgPlot <- renderPlot({
-    with(mpgData, {
-      plot(as.formula(formulaTextPoint()))
-      abline(fit(), col=2)
-    })
-  })
-  
 })
